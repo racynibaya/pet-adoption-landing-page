841614553ee151e81c4328e9009dacc8cd1a26b4
Racyn Ibaya <racynibaya@outlook.com>
2024-03-01 11:38:48 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const heroSectionEl = document.querySelector(".hero-section");
+
+const obs = new IntersectionObserver(
+  (entries) => {
+    const entry = entries[0];
+    console.log(entry);
+
+    const { isIntersecting } = entry;
+
+    if (!isIntersecting) {
+      document.querySelector(".header").classList.add("sticky");
+    } else {
+      document.querySelector(".header").classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    rootMargin: "-80px",
+    threshold: 0,
+  }
+);
+
+obs.observe(heroSectionEl);
